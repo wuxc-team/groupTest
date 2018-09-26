23416e00f8e98dffd686b5565bd090e5055e5f13
wuxc0204 <944672412@qq.com>
2018-09-26 11:15:33 +0800

    hhgfhgfhfghf

--- groupTest/src/com/example/grouptest/MainActivity.java
+++ groupTest/src/com/example/grouptest/MainActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-private String test;
+ 
 public class MainActivity extends ActionBarActivity {
 	private String intr;
 	private String intr2;
